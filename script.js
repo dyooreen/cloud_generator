aa22191dd6c0cef59d944f176395f06167445efb
sandaramet <aram@gratun.am>
2020-04-29 18:13:26 +0400

    Code Refactoring

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-var xoff = 0;
-var yoff = 0;
-// var start = 0;
-var inc = 0.01;
-let resolution = 1
-let red, green, blue
+
+const inc = 0.01;
+const resolution = 1
+const color = [0, 0, 255, 255] //RGBA
+const nOctaves = 8 // noiseDetail() first parameter
+const nDetail = 0.5 // noiseDetail() second parameter
+let xoff = 0;
+let yoff = 0;
 function setup() {
-
-
-    red = 0
-    green = 0
-    blue = 255
-
     createCanvas(windowWidth, windowHeight);
-    background(255)
-    noiseDetail(8)
+    pixelDensity(1);
+    noiseDetail(nOctaves, nDetail)
     loadPixels()
-    for (var y = 0; y < height; y += resolution) {
+    for (let y = 0; y < height; y += resolution) {
         xoff = 0
-        for (var x = 0; x < width; x += resolution) {
-
-            var i = (x+y*width)*4;
-			pixels[i+0] = cos(y/(width*2))*red
-			pixels[i+1] = cos(y/(width*2))*green;
-			pixels[i+2] = cos(y/(width*2))*blue;
-  			pixels[i+3] = noise(xoff, yoff)*255;
+        for (let x = 0; x < width; x += resolution) {
+            var i = (x + y * width) * 4;
+            pixels[i + 0] = cos(y / (width * 2)) * color[0] // Red
+            pixels[i + 1] = cos(y / (width * 2)) * color[1] // Green
+            pixels[i + 2] = cos(y / (width * 2)) * color[2] // Blue
+            pixels[i + 3] = noise(xoff, yoff) * color[3]; // Alpha
             xoff += inc;
         }
         yoff += inc;
